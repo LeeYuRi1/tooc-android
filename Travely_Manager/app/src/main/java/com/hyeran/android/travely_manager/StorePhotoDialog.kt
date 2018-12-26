fdd9443e5b6ebbd74577764011906be81245af40
HyeranShin <hyebaragi__@naver.com>
2018-12-27 00:09:23 +0900

    permissioin check 함수 추가

--- Travely_Manager/app/src/main/java/com/hyeran/android/travely_manager/StorePhotoDialog.kt
+++ Travely_Manager/app/src/main/java/com/hyeran/android/travely_manager/StorePhotoDialog.kt
@@ -9,7 +9,7 @@ import android.view.Window
 import kotlinx.android.synthetic.main.dialog_store_photo.*
 
 class StorePhotoDialog(val ctx : Context?) : Dialog(ctx) {
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
