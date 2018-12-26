9c7c42022f09fb7f2a514b66ce88d90c736d7ed5
haewon96 <eunhae96@naver.com>
2018-12-26 21:43:51 +0900

    MapFragment 지도가 띄워지도록 버그 수정, 현재위치 업데이트

--- Travely_User/app/src/main/java/com/hyeran/android/travely_user/MapMoreFragment.kt
+++ Travely_User/app/src/main/java/com/hyeran/android/travely_user/MapMoreFragment.kt
@@ -28,22 +28,27 @@ class MapMoreFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-       // setRecyclerView()
+        setViewPager()
+        setRecyclerView()
+    }
+
+    private fun setViewPager() {
+
     }
 
     private fun setRecyclerView() {
-        // 임시 데이터 1
-        val dataList1: ArrayList<PhotoData> = ArrayList()
-        dataList1.add(PhotoData("filter1"))
-        dataList1.add(PhotoData("filter2"))
-        dataList1.add(PhotoData("filter3"))
-        dataList1.add(PhotoData("filter4"))
-        dataList1.add(PhotoData("filter5"))
-        dataList1.add(PhotoData("filter6"))
-
-        photoRecyclerViewAdapter = PhotoRecylerViewAdapter(activity!!, dataList1)
-        rv_main_frag_photo_list.adapter = photoRecyclerViewAdapter
-        rv_main_frag_photo_list.layoutManager = GridLayoutManager(activity, 3)
+//        // 임시 데이터 1
+//        val dataList1: ArrayList<PhotoData> = ArrayList()
+//        dataList1.add(PhotoData("filter1"))
+//        dataList1.add(PhotoData("filter2"))
+//        dataList1.add(PhotoData("filter3"))
+//        dataList1.add(PhotoData("filter4"))
+//        dataList1.add(PhotoData("filter5"))
+//        dataList1.add(PhotoData("filter6"))
+//
+//        photoRecyclerViewAdapter = PhotoRecylerViewAdapter(activity!!, dataList1)
+//        rv_main_frag_photo_list.adapter = photoRecyclerViewAdapter
+//        rv_main_frag_photo_list.layoutManager = GridLayoutManager(activity, 3)
 
         // 임시 데이터 2
         val dataList2: ArrayList<ReviewData> = ArrayList()
@@ -56,7 +61,7 @@ class MapMoreFragment : Fragment() {
                 "사장님이 굉장히 친절하고 덕분에 편안하게 여행했어요 최고~!!"))
 
         reviewRecyclerViewAdapter = ReviewRecyclerViewAdapter(activity!!, dataList2)
-        rv_main_frag_map_list.adapter = reviewRecyclerViewAdapter
-        rv_main_frag_map_list.layoutManager = LinearLayoutManager(activity)
+        rv_map_more_frag_review_list.adapter = reviewRecyclerViewAdapter
+        rv_map_more_frag_review_list.layoutManager = LinearLayoutManager(activity)
     }
 }
---
+++ Travely_User/app/src/main/java/com/hyeran/android/travely_user/PagerModel.kt
@@ -0,0 +1,13 @@
+package com.hyeran.android.travely_user
+
+class PagerModel {
+    private var images : Int = 0
+
+    fun getImages() : Int {
+        return images
+    }
+
+    fun setImages (images : Int) {
+        this.images = images
+    }
+}
