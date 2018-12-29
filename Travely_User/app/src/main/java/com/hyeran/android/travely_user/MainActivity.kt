dc57abbac21ec384bcd2713e3a7811768c459e08
HyeranShin <hyebaragi__@naver.com>
2018-12-30 01:16:43 +0900

    하단 탭바 디자인, Selector 적용

--- Travely_User/app/src/main/java/com/hyeran/android/travely_user/MainActivity.kt
+++ Travely_User/app/src/main/java/com/hyeran/android/travely_user/MainActivity.kt
@@ -8,6 +8,7 @@ import com.hyeran.android.travely_user.map.MapFragment
 import com.hyeran.android.travely_user.mypage.MypageFragment
 import com.hyeran.android.travely_user.reserve.ReserveFragment
 import com.hyeran.android.travely_user.reserve_state.ReserveStateFragment
+import com.hyeran.android.travely_user.ship.ShipFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,29 +18,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         addFragment(MapFragment.getInstance())
+        iv_search_bottom_tab.isSelected = true
         setOnClickListener()
     }
 
     fun setOnClickListener() {
         tab_one_main.setOnClickListener {
+            clearSelected()
+            iv_search_bottom_tab.isSelected = true
             replaceFragment(MapFragment.getInstance())
         }
         tab_two_main.setOnClickListener {
-             replaceFragment(ReserveStateFragment())
-
-//            val fm = supportFragmentManager
-//            val dialogFragment = TestTestTestTestTest()
-//            dialogFragment.show(fm, "fragment_dialog_test")
-
-
+            clearSelected()
+            iv_reserve_bottom_tab.isSelected = true
+            replaceFragment(ReserveStateFragment())
         }
         tab_three_main.setOnClickListener {
-            //            replaceFragment(ShipFragment())
-            replaceFragment(ReserveFragment())
+            clearSelected()
+            iv_ship_bottom_tab.isSelected = true
+            replaceFragment(ShipFragment())
+//            replaceFragment(ReserveFragment())
         }
         tab_four_main.setOnClickListener {
+            clearSelected()
+            iv_mypage_bottom_tab.isSelected = true
             replaceFragment(MypageFragment())
         }
+    }
+
+    fun clearSelected() {
+        iv_search_bottom_tab.isSelected = false
+        iv_reserve_bottom_tab.isSelected = false
+        iv_ship_bottom_tab.isSelected = false
+        iv_mypage_bottom_tab.isSelected = false
     }
 
     fun addFragment(fragment: Fragment) {
