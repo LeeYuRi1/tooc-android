665a3b43a20606d33169cceedf9440f45d0bad13
HyeranShin <hyebaragi__@naver.com>
2019-01-03 16:59:31 +0900

    Fix_맵 플로우 다듬기, 데이터 전달 하는 중

--- Travely_User/app/src/main/java/com/hyeran/android/travely_user/adapter/MoreLocationRVAdapter.kt
+++ Travely_User/app/src/main/java/com/hyeran/android/travely_user/adapter/MoreLocationRVAdapter.kt
@@ -1,6 +1,7 @@
 package com.hyeran.android.travely_user.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.hyeran.android.travely_user.R
 import android.util.Log
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.Toast
 import com.hyeran.android.travely_user.map.LocationListActivity
@@ -28,12 +30,14 @@ class MoreLocationRVAdapter(val ctx : Context, val dataList: ArrayList<SimpleSto
         holder.name.text = dataList[position].storeName
         Log.d("MoreLocationRVAdapter", "@@@@@@@@@@@@@"+dataList[position])
         holder.item_more_location_map.setOnClickListener {
+            val intent : Intent = Intent()
+            intent.putExtra("storeIdx", dataList[position].storeIdx)
             (ctx as LocationListActivity).finish()
         }
     }
 
     inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val name : TextView = itemView.findViewById(R.id.tv_name_item_more_location_map) as TextView
-        val item_more_location_map : RelativeLayout = itemView.findViewById(R.id.item_more_location_map) as RelativeLayout
+        val item_more_location_map : LinearLayout = itemView.findViewById(R.id.item_more_location_map) as LinearLayout
     }
 }
