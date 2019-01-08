8d25f680cfc385addf250b886568cbc9edd6654b
HyeranShin <hyebaragi__@naver.com>
2019-01-08 11:58:43 +0900

    마이페이지 통신 연결 중

--- Travely_Manager/app/src/main/java/com/hyeran/android/travely_manager/network/NetworkService.kt
+++ Travely_Manager/app/src/main/java/com/hyeran/android/travely_manager/network/NetworkService.kt
@@ -1,11 +1,23 @@
 package com.hyeran.android.travely_manager.network
 
+import com.google.gson.JsonObject
 import com.hyeran.android.travely_manager.model.MypageResponseData
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface NetworkService{
+
+    // 로그인
+    // - 성공 시 jwt 토큰을 헤더에 넣어서 반환
+    @POST("/api/users/login")
+    fun postLoginResponse(
+            @Header("Content-Type") content_type: String,
+            @Body() body: JsonObject
+    ): Call<Any>
+
     // 관리자 마이페이지
     @GET("/api/owner/mypage")
     fun getMypageResponse(
