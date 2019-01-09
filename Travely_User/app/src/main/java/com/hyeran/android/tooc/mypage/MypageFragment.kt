9fc8bf19fa27021aa37f2d9e36cfc59e80a505ff
leeyuri <dbf5156@naver.com>
2019-01-09 23:45:06 +0900

    faq 수정, 관리자 - 예약상세 뷰 레이아웃 수정

--- Travely_User/app/src/main/java/com/hyeran/android/tooc/mypage/MypageFragment.kt
+++ Travely_User/app/src/main/java/com/hyeran/android/tooc/mypage/MypageFragment.kt
@@ -163,16 +163,12 @@ class MypageFragment : Fragment() {
 //                                var position = mypageRecentStoreAdapter.itemCount
 //                                mypageRecent = dataList_recent
 //                                mypageRecentStoreAdapter.notifyItemInserted(position)
-//
+
 //                                var position = mypageRecentStoreAdapter.itemCount
 //                                //mypageRecentStoreAdapter.dataList.clear()
 //                                mypageRecentStoreAdapter.dataList.addAll(dataList_recent)
 //                                mypageRecentStoreAdapter.notifyItemInserted(position)
 
-                                var position = mypageRecentStoreAdapter.itemCount
-                                mypageRecentStoreAdapter.dataList.clear()
-                                mypageRecentStoreAdapter.dataList.addAll(dataList_recent)
-                                mypageRecentStoreAdapter.notifyItemChanged(position)
                             } else {
 
                             }
